246e4ac826fb0bca0a867833e36760d8ad867e15
Pedro Martins <mourao.martins@gmail.com>
2023-07-05 16:50:17 +0100

    Add Init

---
+++ Book-Guide-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Book_Guide_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
