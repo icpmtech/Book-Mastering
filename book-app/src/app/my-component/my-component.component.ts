d02fffbbb1c8d14ceca1362162398973050e1161
Pedro Martins <mourao.martins@gmail.com>
2023-07-11 20:57:39 +0100

    Add service to my components service

--- book-app/src/app/my-component/my-component.component.ts
+++ book-app/src/app/my-component/my-component.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
-  styleUrls: ['./my-component.component.less']
 })
 export class MyComponentComponent {
-  title = 'Hello, world!';
 
-  sayHello() {
-    alert('Hello, world!');
-  }
+  constructor(private userService: UserService) { }
+
 }
