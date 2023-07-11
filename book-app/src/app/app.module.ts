f4420b46361614dd46825805ce3961730216483c
Pedro Martins <mourao.martins@gmail.com>
2023-07-11 22:59:14 +0100

    add user

--- book-app/src/app/app.module.ts
+++ book-app/src/app/app.module.ts
@@ -6,16 +6,19 @@ import { MyComponentComponent } from './my-component/my-component.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RouterModule,Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'user/:id', component: UserComponent },
 ];
 @NgModule({
   declarations: [
     AppComponent,
     MyComponentComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
